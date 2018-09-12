2bda0bdd4f89977c8116d5bd6d60a1e09435a10a
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-12 16:34:42 -0700

    intro component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import FadeIn from 'react-fade-in';
 import './App.css';
+import Intro from './Intro'
 
 class App extends Component {
 
@@ -8,22 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id="top" className="snow">
-          <div id="fade" className="greeting">
-            <FadeIn delay={300}>
-              <div>
-                <p className="intro-p">HELLO, I'M <span className="name"> ERINÇ EMER</span>. </p>
-              </div>
-              <div>
-                <p className="intro-p">I SOLVE PROBLEMS IN</p>
-              </div>
-              <div>
-                <p className="intro-p">ELEGANT WAYS.</p>
-              </div>
-            </FadeIn>
-          </div>
-          <div className="arrow bounce"></div>
-        </div>
+        <Intro/>
       </div>
     );
   }
